ecc77f5da5a722fb0ff1872b66dc6ae6e41738d6
Dan Fried <daniel.k.fried@gmail.com>
2013-04-16 13:57:34 -0400

    Separate environment and server_type

--- lib/capistrano/ec2tag.rb
+++ lib/capistrano/ec2tag.rb
@@ -1,5 +1,5 @@
 require 'aws-sdk'
-
+require 'capistrano/ext/multistage'
 unless Capistrano::Configuration.respond_to?(:instance)
   abort 'capistrano/ec2tag requires Capistrano >= 2'
 end
@@ -8,10 +8,9 @@ module Capistrano
   class Configuration
     module Tags
 
-      def tag(which, *args)
+      def tag(environment, server_type, *args)
         @ec2 ||= AWS::EC2.new({access_key_id: fetch(:aws_access_key_id), secret_access_key: fetch(:aws_secret_access_key)}.merge! fetch(:aws_params, {}))
-
-        @ec2.instances.filter('tag-key', 'deploy').filter('tag-value', which).each do |instance|
+        @ec2.instances.filter('tag-key', 'environment').filter('tag-value', environment).filter('tag-key', 'server_type').filter('tag-value', server_type).each do |instance|
           server instance.dns_name || instance.ip_address, *args if instance.status == :running
         end
       end
