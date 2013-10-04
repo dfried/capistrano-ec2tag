a39ed46e075f568f5ec4b27a4365fb91d78895b0
Dan Fried <daniel.k.fried@gmail.com>
2013-10-04 19:18:45 -0400

    Refactored to add no_release support

--- lib/capistrano/ec2tag.rb
+++ lib/capistrano/ec2tag.rb
@@ -2,21 +2,18 @@ require 'aws-sdk'
 # require 'capistrano/ext/multistage'
 unless Capistrano::Configuration.respond_to?(:instance)
   abort 'capistrano/ec2tag requires Capistrano >= 2'
-end
+end 
 
 module Capistrano
   class Configuration
     module Tags
-
-      def tag(environment, server_type, *args)
+      def tag(server_type, environment, *args)
         @ec2 ||= AWS::EC2.new({access_key_id: fetch(:aws_access_key_id), secret_access_key: fetch(:aws_secret_access_key)}.merge! fetch(:aws_params, {}))
         @ec2.instances.filter('tag-key', 'environment').filter('tag-value', environment).filter('tag-key', 'server_type').filter('tag-value', server_type).each do |instance|
-          server instance.dns_name || instance.ip_address, *args if instance.status == :running
+          role(server_type, instance.dns_name || instance.ip_address, *args) if instance.status == :running
         end
       end
-
     end
-
     include Tags
   end
 end
